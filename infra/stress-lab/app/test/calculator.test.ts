90a618eb077b40442aee4bf6220bca92385a6376
Johan-py <johanenvdev@gmail.com>
2026-03-18 20:50:16 -0400

    fix: stress lab

--- infra/stress-lab/app/test/calculator.test.ts
+++
@@ -1,5 +0,0 @@
-import { multiply } from './../utils'
-
-test('multiply 3*5 = 15', () => {
-  expect(multiply(3, 5)).toBe(999)
-})
--- infra/stress-lab/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Aquí puedes agregar configuraciones específicas para stress-lab
-}
-
-export default nextConfig
